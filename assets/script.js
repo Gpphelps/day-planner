2c86f30098f5f70417d0cf1b639a9d6fb8c1a1d9
Gene Phelps <gpphelpsii@gmail.com>
2021-03-06 16:27:05 -0500

    edited current date and time strted JS function for changing hour blocks different colors

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,37 @@
-var currentDay;
-// Constant for the dark mode/light mode switch
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-// Retireves the current system time and date and sets the text of the p element with the id=currentDay to be the current system time and date
+// Retireves the current system time and date and sets the text element with the id=currentDay to be the current system time and date
 setInterval(function() {
-    currentDay = moment(); 
-    $("#currentDay").text(currentDay.format("MMM Do, YYYY [at] hh:mm:ss"));
+    var currentDay = moment(); 
+    $("#currentDate").text(currentDay.format("dddd,  MMMM Mo, YYYY [at] hh:mm:ss a"));
 }, 1000);
 
+// Reference for DOM elements needed to change color of the userInput based upon time
+var calandarBlock8= $("#userInput8");
+var calandarBlock9= $("#userInput9");
+var calandarBlock10= $("#userInput10");
+var calandarBlock11= $("#userInput11");
+var calandarBlock12= $("#userInput12");
+var calandarBlock1= $("#userInput1");
+var calandarBlock2= $("#userInput2");
+var calandarBlock3= $("#userInput3");
+var calandarBlock4= $("#userInput4");
+var calandarBlock5= $("#userInput5");
+
+checkTime(function() {
+
+}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+// Constant for the dark mode/light mode switch
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 //Changes the webpages visual theme between dark and light mode
 function switchTheme(e) {
@@ -22,3 +46,4 @@ function switchTheme(e) {
 }
 // Event listner for the dark mode/light mode toggle switch
 toggleSwitch.addEventListener('change', switchTheme, false);
+
