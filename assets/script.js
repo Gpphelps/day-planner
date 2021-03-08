a66be2e4314309df7243fc6a6aca71b99a52ab13
Gene Phelps <gpphelpsii@gmail.com>
2021-03-08 16:07:11 -0500

    added saveBtn image to HTML, organized JS, added JS functionality to save the user's visual theme to the local storage and retieve it upon loading of the webpage

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,19 @@ $(document).ready(function () {
         var userAppiontment = $("#" + keys[i]).find("textarea");
         userAppiontment.val(value);
     }
-    
+});
+
+// jQuery functions that fires on click of the save buttons that stores the user's input from the sibling text area to the value in local storage and the id of the parent row as the key value in local storage 
+$(".saveBtn").on("click", function () {
+    var value = $(this).siblings(".userInput").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, value);
+});
+
+$(".btn5").on("click", function () {
+    var value = $(this).siblings(".userInput5").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, value);
 });
 
 // Reference for DOM elements needed to change color of the userInput based upon time
@@ -26,7 +38,7 @@ setInterval(function() {
     var currentDay = moment(); 
     $("#currentDate").text(currentDay.format("dddd,  MMMM Mo, YYYY [at] hh:mm:ss a"));
 
-    // if loop that checks to see what hour it currently is in military (24 hour) time and adds a class to the timeblock depending on whether the given time block is after, during, or before the current hour. The values for "H" can be 0-24.
+    // if loops that checks to see what hour it currently is in military (24 hour) time and adds a class to the timeblock depending on whether the given time block is after, during, or before the current hour. The values for "H" can be 0-24.
     if ((moment().format("H")) > 8 ) {
         $(calandarBlock8).addClass("past");
     } 
@@ -138,33 +150,35 @@ setInterval(function() {
     }
 }, 1000);
 
-// jQuery functions that fires on a click of the save buttons that stores the user's input from the sibling text area to the value in local storage and the id of the parent row as the key value in local storage 
-$(".saveBtn").on("click", function () {
-    var value = $(this).siblings(".userInput").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, value);
-});
-
-$(".btn5").on("click", function () {
-    var value = $(this).siblings(".userInput5").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, value);
-});
-
-// Constant for the dark mode/light mode switch
+// Constant for the dark mode/light mode switch which is checkbox styled to look like a toggle switch
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-//Changes the webpages visual theme between dark and light mode
+//Changes the webpages visual theme between dark and light mode and saves the current theme to the user's local storage
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         document.getElementById("switchText").textContent= "Toggle Light Mode";
+        localStorage.setItem('theme', 'dark');
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
         document.getElementById("switchText").textContent= "Toggle Dark Mode";
+        localStorage.setItem('theme', 'light');
     }    
 }
+
+// Constant that is set to be the value of the current theme from local storage
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+// If loops that check the currentTheme constant  and sets the data-theme of the webpage to be the currentTheme upon loading of the webpage
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
 // Event listner for the dark mode/light mode toggle switch
 toggleSwitch.addEventListener('change', switchTheme, false);
 
